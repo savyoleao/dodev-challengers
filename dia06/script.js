4370810080f827a316bf265695402d659a35a79d
Savyo Leao <savyoleaoo@gmail.com>
2024-01-19 15:52:10 -0300

    Desafios dos dias 6, 8 e 11

---
+++ dia06/script.js
@@ -0,0 +1,5 @@
+let valor = Number(prompt('insira o valor para contagem de 5 em 5'))
+
+for(let i = 0; i <= valor; i-=5){
+   console.log(i)
+}
---
+++ dia08/script.js
@@ -0,0 +1,6 @@
+let array = []
+
+for(let i = 0; i < 5; i++){
+   let num = Number(prompt('insira 5 numeros: '));
+   array[i] = num;
+}
