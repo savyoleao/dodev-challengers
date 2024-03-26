cd807d672423b49be8f82f6c18f6ca54a260e7f6
Savyo Leao <savyoleaoo@gmail.com>
2024-03-26 15:42:31 -0300

    evento de focus e blur

--- front-end/eventos/script.js
+++ front-end/eventos/script.js
@@ -5,3 +5,12 @@ button.addEventListener('submit', (event)=>{ //evento do tipo submit
    alert('recebi o evento');
    console.log(event);
 })
+
+const input = document.querySelector('#emailtype');
+input.addEventListener('focus', () => { //evento de foco no elemento input
+   console.log('focou no elemento')
+})
+
+input.addEventListener('blur', () => { //evento de desfocar no elemento input (validação de email)
+   console.log('desfocou o elemento')
+})
