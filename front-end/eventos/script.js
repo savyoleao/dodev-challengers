363554455aea140aa6a23b90013d17e7957630af
Savyo Leao <savyoleaoo@gmail.com>
2024-03-26 16:20:48 -0300

    event mouseup mousedown mouseover mouseout

--- front-end/eventos/script.js
+++ front-end/eventos/script.js
@@ -22,3 +22,19 @@ document.addEventListener('scroll', () => { //descer uma página (pop-ups)
 document.addEventListener('click', () => {
    document.body.style.backgroundColor = "white"
 })
+
+window.addEventListener('resize', () => { //mudar o tamanho da página
+   console.log('mudou o tamanho')
+})
+
+const btn = document.querySelector('#mouseTeste');
+
+btn.addEventListener('mouseup', () =>{//solta o clique do mouse
+   btn.style.backgroundColor = "#F0F0F0"
+})
+btn.addEventListener('mousedown', () =>{//segura o clique do mouse
+   btn.style.backgroundColor = "red"
+})
+btn.addEventListener('mouseover', () =>{//passa o mouse encima
+   btn.style.backgroundColor = "yellow"
+})
