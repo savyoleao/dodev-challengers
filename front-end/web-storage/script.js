9fa01a30ae92ac0d1a9364c7bef889faf0f61b8d
Savyo Leao <savyoleaoo@gmail.com>
2024-03-27 23:14:26 -0300

    web storage: sessionStorage e localStorage

--- front-end/web-storage/script.js
+++ front-end/web-storage/script.js
@@ -6,3 +6,11 @@ const input = document.querySelector('#nameText')
 input.addEventListener('blur', (event) => {
    save(event.target.value)
 }); //ao desfocar do input ele salva o texto do mesmo
+
+//existe tambem o localstorage que salva as informações mesmo se a página for fechada
+
+//MÉTODOS (vale para localstorage e sessionstorage)
+window.localStorage.setItem('key', 'value') //armazena o valor
+window.sessionStorage.getItem('key') //busca o valor
+window.sessionStorage.removeItem('key')// remove um valor
+window.localStorage.clear()//limpa tudo
